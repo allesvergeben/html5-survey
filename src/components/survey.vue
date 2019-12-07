6c7b30afce8a3ae704fbbf5537d002a9566d1f19
laurin-d <laurin.doerre@lgs-hu.eu>
2019-12-07 14:42:54 +0100

    initial commit

---
+++ src/components/survey.vue
@@ -0,0 +1,14 @@
+<template>
+    <v-container>
+        <p>{{ lang }}</p>
+    </v-container>
+</template>
+
+<script>
+    export default {
+        name: 'Survey',
+        props: {
+            lang: String
+        }
+    }
+</script>
